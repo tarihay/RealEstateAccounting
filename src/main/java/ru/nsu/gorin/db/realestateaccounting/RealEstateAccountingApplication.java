5861136253811ccf547daf8ec84d2aa49c43beb4
i.gorin <i.gorin@g.nsu.ru>
2023-04-26 23:11:45 +0700

    project init commit

---
+++ src/main/java/ru/nsu/gorin/db/realestateaccounting/RealEstateAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.gorin.db.realestateaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateAccountingApplication.class, args);
+    }
+
+}
